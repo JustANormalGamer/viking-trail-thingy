ad50fbdbe38c172d9e708680bf08f0a4cdff8516
JustANormalGamer <sonicmaster6@hotmail.com>
2015-04-15 10:53:37 +0200

    buttons og funksjon for button
    funksjon for button skal flyttes til class

--- Tekstlogg og menyvalg test/Tekstlogg og menyvalg test/Form1.Designer.cs
+++ Tekstlogg og menyvalg test/Tekstlogg og menyvalg test/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonNW = new System.Windows.Forms.Button();
+            this.buttonNE = new System.Windows.Forms.Button();
+            this.buttonSW = new System.Windows.Forms.Button();
+            this.buttonSE = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,11 +46,55 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // buttonNW
+            // 
+            this.buttonNW.Location = new System.Drawing.Point(393, 622);
+            this.buttonNW.Name = "buttonNW";
+            this.buttonNW.Size = new System.Drawing.Size(473, 58);
+            this.buttonNW.TabIndex = 1;
+            this.buttonNW.Text = "Ja";
+            this.buttonNW.UseVisualStyleBackColor = true;
+            this.buttonNW.Click += new System.EventHandler(this.buttonNW_Click);
+            // 
+            // buttonNE
+            // 
+            this.buttonNE.Location = new System.Drawing.Point(867, 622);
+            this.buttonNE.Name = "buttonNE";
+            this.buttonNE.Size = new System.Drawing.Size(473, 58);
+            this.buttonNE.TabIndex = 2;
+            this.buttonNE.Text = "Nei";
+            this.buttonNE.UseVisualStyleBackColor = true;
+            this.buttonNE.Click += new System.EventHandler(this.buttonNE_Click);
+            // 
+            // buttonSW
+            // 
+            this.buttonSW.Location = new System.Drawing.Point(393, 681);
+            this.buttonSW.Name = "buttonSW";
+            this.buttonSW.Size = new System.Drawing.Size(473, 58);
+            this.buttonSW.TabIndex = 3;
+            this.buttonSW.Text = "Handle";
+            this.buttonSW.UseVisualStyleBackColor = true;
+            this.buttonSW.Click += new System.EventHandler(this.buttonSW_Click);
+            // 
+            // buttonSE
+            // 
+            this.buttonSE.Location = new System.Drawing.Point(867, 681);
+            this.buttonSE.Name = "buttonSE";
+            this.buttonSE.Size = new System.Drawing.Size(473, 58);
+            this.buttonSE.TabIndex = 4;
+            this.buttonSE.Text = "plyndre";
+            this.buttonSE.UseVisualStyleBackColor = true;
+            this.buttonSE.Click += new System.EventHandler(this.buttonSE_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1366, 768);
+            this.Controls.Add(this.buttonSE);
+            this.Controls.Add(this.buttonSW);
+            this.Controls.Add(this.buttonNE);
+            this.Controls.Add(this.buttonNW);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
@@ -60,6 +108,10 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonNW;
+        private System.Windows.Forms.Button buttonNE;
+        private System.Windows.Forms.Button buttonSW;
+        private System.Windows.Forms.Button buttonSE;
     }
 }
 
--- Tekstlogg og menyvalg test/Tekstlogg og menyvalg test/Form1.cs
+++ Tekstlogg og menyvalg test/Tekstlogg og menyvalg test/Form1.cs
@@ -16,5 +16,51 @@ namespace Tekstlogg_og_menyvalg_test
         {
             InitializeComponent();
         }
+
+        private void buttonNW_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonSW_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonNE_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonSE_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button_check(int a) 
+        {
+            // når du skal bruke denne for å velge antall knapper skriver du: button_check(); og inne i parantesen antall buttons du vil ha
+            if (a == 2) 
+            {
+                buttonSW.Enabled = false;
+                buttonSE.Enabled = false;
+                buttonSW.Visible = false;
+                buttonSE.Visible = false;
+            }
+            if (a == 3)
+            {
+                buttonSW.Enabled = true;
+                buttonSE.Enabled = false;
+                buttonSW.Visible = true;
+                buttonSE.Visible = false;
+            }
+            if (a == 4)
+            {
+                buttonSW.Enabled = true;
+                buttonSE.Enabled = true;
+                buttonSW.Visible = true;
+                buttonSE.Visible = true;
+            }
+        }
     }
 }
