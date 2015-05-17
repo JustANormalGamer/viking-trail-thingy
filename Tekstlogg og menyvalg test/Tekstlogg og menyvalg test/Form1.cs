ef55e1dcf9c1cb2a7d6818cf26353b01a2d19480
Sander Bjerknes <sander.bjerknes@hotmail.com>
2015-05-17 13:07:29 +0200

    plyndre

--- Tekstlogg og menyvalg test/Tekstlogg og menyvalg test/Form1.cs
+++ Tekstlogg og menyvalg test/Tekstlogg og menyvalg test/Form1.cs
@@ -15,9 +15,24 @@ namespace Tekstlogg_og_menyvalg_test
 {
     public partial class Form1 : Form
     {
+        //ludvig
+        bool upb = false;
+        bool downb = false;
+        bool rightb = false;
+        bool leftb = false;
+        int liv = 50;
+        int nedtelling = 45;
+        int pengerMax = 420;
+        int PengerHar = 0;
+
         public Form1()
         {
             InitializeComponent();
+            KeyDown += new KeyEventHandler(Form1_KeyDown);
+            KeyUp += new KeyEventHandler(Form1_KeyUp);
+            timer1.Start();
+            CountDown.Start();
+            timer2.Start();
         }
 
         //julian
@@ -258,5 +273,312 @@ namespace Tekstlogg_og_menyvalg_test
             panel1.Hide();
         }
         #endregion Eirik
+
+        //Ludvig sine funksjoner
+        #region Ludvig
+
+      //  PictureBox[] p = new PictureBox[20];
+ 
+  private void Form1_KeyDown(object sender, KeyEventArgs e)
+  {
+
+
+      if (e.KeyCode == Keys.Right) rightb = true;
+      if (e.KeyCode == Keys.Left) leftb = true;
+      if (e.KeyCode == Keys.Up) upb = true;
+      if (e.KeyCode == Keys.Down) downb = true;
+ 
+   
+  }
+
+  private void Form1_KeyUp(object sender, KeyEventArgs e)
+  {
+      if (e.KeyCode == Keys.Right) rightb = false;
+      if (e.KeyCode == Keys.Left) leftb = false;
+      if (e.KeyCode == Keys.Up) upb = false;
+      if (e.KeyCode == Keys.Down) downb = false;
+  }
+
+  private void timer1_Tick(object sender, EventArgs e)
+  {
+      int x = pictureBox1.Location.X;
+      int y = pictureBox1.Location.Y;
+
+
+
+      if (upb == true) y -= 1;
+      else if (downb == true) y += 1;
+      else if (rightb == true) x += 1;
+      else if (leftb == true) x -= 1;
+      pictureBox1.Location = new Point(x, y);
+      kollisjonTest(x, y);
+      kiste();
+    
+  }
+
+  private void kollisjonTest(int x, int y)
+  {
+      
+   
+      if (pictureBox1.Bounds.IntersectsWith(pB1.Bounds))
+    {
+        if (upb == true)
+            y += 1;
+        else if (downb == true)
+            y -= 1;
+        else if (leftb == true)
+            x += 1;
+        else if (rightb == true)
+            x -= 1;
+    }
+
+      else if (pictureBox1.Bounds.IntersectsWith(pB2.Bounds))
+      {
+          if (upb == true)
+              y += 1;
+          else if (downb == true)
+              y -= 1;
+          else if (leftb == true)
+              x += 1;
+          else if (rightb == true)
+              x -= 1;
+      }
+      else if (pictureBox1.Bounds.IntersectsWith(pB3.Bounds))
+      {
+          if (upb == true)
+              y += 1;
+          else if (downb == true)
+              y -= 1;
+          else if (leftb == true)
+              x += 1;
+          else if (rightb == true)
+              x -= 1;
+      }
+
+      else if (pictureBox1.Bounds.IntersectsWith(pB4.Bounds))
+      {
+          if (upb == true)
+              y += 1;
+          else if (downb == true)
+              y -= 1;
+          else if (leftb == true)
+              x += 1;
+          else if (rightb == true)
+              x -= 1;
+      }
+
+      else if (pictureBox1.Bounds.IntersectsWith(pB5.Bounds))
+      {
+          if (upb == true)
+              y += 1;
+          else if (downb == true)
+              y -= 1;
+          else if (leftb == true)
+              x += 1;
+          else if (rightb == true)
+              x -= 1;
+      }
+
+      else if (pictureBox1.Bounds.IntersectsWith(pB6.Bounds))
+      {
+          if (upb == true)
+              y += 1;
+          else if (downb == true)
+              y -= 1;
+          else if (leftb == true)
+              x += 1;
+          else if (rightb == true)
+              x -= 1;
+      }
+      else if (pictureBox1.Bounds.IntersectsWith(pictureBoxL1.Bounds))
+      {
+          liv -= 1;
+      }
+      else if (pictureBox1.Bounds.IntersectsWith(pictureBoxL2.Bounds))
+      {
+          liv -= 1;
+      }
+      else if (pictureBox1.Bounds.IntersectsWith(pictureBoxR1.Bounds))
+      {
+          liv -= 1;
+      }
+      else if (pictureBox1.Bounds.IntersectsWith(pictureBoxR2.Bounds))
+      {
+          liv -= 1;
+      }
+
+     
+
+      lbLiv.Text = Convert.ToString(liv);
+      pictureBox1.Location = new Point(x, y);
+      
+  }
+
+        private void kiste()
+  {
+      if (pictureBox1.Bounds.IntersectsWith(pBKiste.Bounds))
+      {
+          Random rnd = new Random();
+          int spawn = rnd.Next(1, 9);
+          
+        if (spawn == 1)
+        {
+            pBKiste.Location = new Point(101, 117);
+        }
+        else if (spawn == 2)
+        {
+            pBKiste.Location = new Point(427, 117);
+
+        }
+        else if (spawn == 3)
+        {
+            pBKiste.Location = new Point(427, 236);
+        }
+        else if (spawn == 4)
+        {
+            pBKiste.Location = new Point( 101, 236);
+        }
+        else if (spawn == 5)
+        {
+            pBKiste.Location = new Point(101, 348);
+        }
+
+        else if (spawn == 6)
+        {
+            pBKiste.Location = new Point(427, 348);
+        }
+        else if (spawn == 7)
+        {
+            pBKiste.Location = new Point(101, 0);
+        }
+        else if (spawn == 8)
+        {
+            pBKiste.Location = new Point(427, 0);
+        }
+        else if (spawn == 9)
+        {
+            pBKiste.Location = new Point(259, 173);
+        }
+        PengerHar += pengerMax / 10;
+        lBpenger.Text = Convert.ToString(PengerHar);
+      }
+      
+  }
+
+  //private void Form1_Load(object sender, EventArgs e)
+  //{
+  //    int x = 50;
+  //    int y = 50;
+  //    for (int i = 0; i < 25; i++)
+  //    {
+  //        p[i] = new PictureBox();
+  //        p[i].Location = new Point(x, y);
+  //        p[i].Size = new Size(100, 50);
+  //        p[i].BackColor = Color.Red;
+  //        p[i].Name = Convert.ToString(i);
+  //        this.Controls.Add(p[i]);
+  //    }
+  //}
+
+  private void pB1_Click(object sender, EventArgs e)
+  {
+
+  }
+
+  private void CountDown_Tick(object sender, EventArgs e)
+  {
+      nedtelling -= 1;
+      label1.Text = Convert.ToString(nedtelling);
+      if (nedtelling == 0)
+      {
+          timer1.Stop();
+          CountDown.Stop();
+          upb = false;
+          downb = false;
+          rightb = false;
+          leftb = false;
+          MessageBox.Show("tid over");  
+      }
+      else if (liv <1)
+      {
+          timer1.Stop();
+          CountDown.Stop();
+          upb = false;
+          downb = false;
+          rightb = false;
+          leftb = false;
+          MessageBox.Show("Du døde"); 
+      }
+  }
+
+  private void label1_Click(object sender, EventArgs e)
+  {
+     
+  }
+
+        
+ // pilene
+
+  private async void timer2_Tick(object sender, EventArgs e)
+  {
+      Random rnd = new Random();
+      int y = rnd.Next(2, 372); // generer et tilfeldig tall mellom 2 and 372
+      int yh = rnd.Next(2, 372);
+      int yv = rnd.Next(2, 372);
+      int yh2 = rnd.Next(2, 372);
+      
+      int x = pictureBoxL1.Location.X;
+      int xh = pictureBoxR1.Location.X;
+      int xv = pictureBoxL2.Location.X;
+      int xh2 = pictureBoxR2.Location.X;
+     
+
+      y = pictureBoxL1.Location.Y;
+      x += 5; 
+
+      yh = pictureBoxR1.Location.Y;
+      xh -= 5;
+
+      yv = pictureBoxL2.Location.Y;
+      xv += 5;
+
+      yh2 = pictureBoxR2.Location.Y;
+      xh2 -= 5;
+                                // farten til pilene
+      
+
+      pictureBoxL1.Location = new Point(x, y);
+      pictureBoxR1.Location = new Point(xh, yh);
+      pictureBoxL2.Location = new Point(xv, yv);
+      pictureBoxR2.Location = new Point(xh2, yh2);
+
+
+      if (x == 492) 
+      {
+          y = rnd.Next(2, 372);
+          yv = rnd.Next(2, 372);
+          pictureBoxL1.Location = new Point(2, y);
+          pictureBoxL2.Location = new Point(2, yv);
+
+      }
+      
+      if (xh == 2)
+      {
+          //await Task.Delay(200);
+          yh = rnd.Next(2, 372);
+          yh2 = rnd.Next(2, 372);
+          pictureBoxR1.Location = new Point(492, yh);
+          pictureBoxR2.Location = new Point(492, yh2);
+          
+      }
+      
+  }
+
+  private void Form1_Load(object sender, EventArgs e)
+  {
+
+  }
+        }
+        #endregion
     }
 }
